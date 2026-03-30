0bed38e06bd1d47a09e8916c8e67731be5bcea74
kelly deal <kellyanndeal@gmail.com>
2026-03-30 12:58:34 -0400

    changed map bc stadia gives 401 error when serving :(

--- js/leafletMap.js
+++ js/leafletMap.js
@@ -20,10 +20,20 @@ class LeafletMap {
     vis.esriAttr =
       "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community";
 
-    vis.stadiaUrl =
-      "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}";
-    vis.stadiaAttr =
-      '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    // vis.stadiaUrl =
+    //   "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}";
+    // vis.stadiaAttr =
+    //   '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+    vis.stadiaUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+    vis.stadiaAttr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+//     var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// 	maxZoom: 19,
+// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// });
+
+
 
     vis.base_layer = L.tileLayer(vis.stadiaUrl, {
       attribution: vis.stadiaAttr,
